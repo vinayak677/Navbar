1744a80226d423b0fe02d8c5169e690c9388f468
vinayak677 <vinayakkoli067@gmail.com>
2023-02-08 15:00:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-function App() {
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="page">
+  <header className='header' tabindex="0">Header</header>
+  <div className='navItems'>
+    <ul className='navlist'> 
+        <li className='navs'><a href="/">Home</a></li>
+        <li className='navs'><a href="#0">Services</a></li>
+        <li className='navs'><a href="#0">Blog</a></li>
+        <li className='navs'><a href="#0">About</a></li>
+        <li className='navs'><a href="#0">Contact</a></li>
+        {/* <li class="small"><a href="#0">Facebook</a><a href="#0">Instagram</a></li> */}
+      </ul>
     </div>
+  <div id="nav-container">
+    <div class="bg"></div>
+    <div class="button" tabindex="0">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </div>
+    <div id="nav-content" tabindex="0">
+      <ul>
+        <li><a href="#0">Home</a></li>
+        <li><a href="#0">Services</a></li>
+        <li><a href="#0">Blog</a></li>
+        <li><a href="#0">About</a></li>
+        <li><a href="#0">Contact</a></li>
+        <li class="small"><a href="#0">Facebook</a><a href="#0">Instagram</a></li>
+      </ul>
+    </div>
+    
+  </div>
+</div>
   );
-}
+};
+
 
 export default App;
