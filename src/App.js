df3a311a99205a91305220e6584d3a105770f14b
vinayak677 <vinayakkoli067@gmail.com>
2023-02-17 20:00:24 +0530

    changes to original

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import React from 'react'
-import Main from './Main'
-import { Route, Routes,BrowserRouter } from 'react-router-dom';
-import Home from './comp/Home';
-import About from './comp/About';
-import Blog from './comp/Blog';
-import Contact from './comp/Contact';
-import Service from './comp/Service';
-import Form from './Form';
-import Navbar from './Navbar';
-
-
-
+import React, { useState } from 'react';
 const App = () => {
-  return (
-    <Main/>
-    // <BrowserRouter>
-    // <Main/>
-    // {/* <Navbar/> */}
-    //   <Routes>
-    //     <Route path="/home" element={<Home />} />
-    //     <Route path="/about" element={<About />} />
-    //     <Route path="/blog" element={<Blog />} />
-    //     <Route path="/contact" element={<Contact />} />
-    //     <Route path="/service" element={<Service />} />
-    //     <Route path="/" element={<Form />} />
-    //   </Routes>
-    // </BrowserRouter>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default App
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div class="page">
+  <header className='header' tabindex="0">Header</header>
+  <div className='navItems'>
+    <ul className='navlist'> 
+        <li className='navs'><a href="/">Home</a></li>
+        <li className='navs'><a href="#0">Services</a></li>
+        <li className='navs'><a href="#0">Blog</a></li>
+        <li className='navs'><a href="#0">About</a></li>
+        <li className='navs'><a href="#0">Contact</a></li>
+        {/* <li class="small"><a href="#0">Facebook</a><a href="#0">Instagram</a></li> */}
+      </ul>
+    </div>
+  <div id="nav-container">
+    <div class="bg"></div>
+    <div class="button" tabindex="0">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </div>
+    <div id="nav-content" tabindex="0">
+      <ul>
+        <li><a href="#0">Home</a></li>
+        <li><a href="#0">Services</a></li>
+        <li><a href="#0">Blog</a></li>
+        <li><a href="#0">About</a></li>
+        <li><a href="#0">Contact</a></li>
+        <li class="small"><a href="#0">Facebook</a><a href="#0">Instagram</a></li>
+      </ul>
+    </div>
+    
+  </div>
+</div>
+  );
+};
+
+
+export default App;
